3d321f77919284a240e2bb6cc99e8809a11ab856
杨涛 <spryti@qq.com>
2016-12-11 12:28:50 +0800

    优化结构

---
+++ bin/um.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+var Liftoff = require('liftoff');
+var argv = require('minimist')(process.argv.slice(2));
+
+var cli = new Liftoff({
+    name: 'fis-um',
+    processTitle: 'fis-um',
+    moduleName: 'fis-um',
+    configName: 'config',
+    extensions: {
+        '.js': null
+    }
+});
+
+cli.launch({
+    cwd: argv.r || argv.root,
+    configPath: argv.f || argv.file
+}, function (env) {
+    var fis, config;
+
+    try {
+        config = require(env.configPath);
+    } catch (e) {
+        config = {fis: true};
+    }
+    fis = require('../')(config);
+
+    if (config.isMin)
+        argv.w = false;
+
+    process.title = this.name + ' ' + process.argv.slice(2).join(' ') + ' [ ' + env.cwd + ' ]';
+    fis.cli.name = this.name;
+    fis.cli.run(argv, env);
+});
